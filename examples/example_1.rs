c5ea36ee95dc9735c80efaa12fa18d5f8c37c7b3
sir Random <random@amberskynet.org>
2025-06-29 17:59:28 +0300

    update example for custom log config

--- examples/example_1.rs
+++ examples/example_1.rs
@@ -8,10 +8,18 @@ fn main() {
 
 #[cfg(feature = "test_messages")]
 fn main() {
-    let c = AsnLogConfig {
+    let main_module_name = "main()";
+
+    let mut c = AsnLogConfig {
         global_level: AsnLogLevel::Trace,
         module_levels: Default::default(),
     };
 
+    c.module_levels
+        .insert(String::from(main_module_name), AsnLogLevel::Warn);
+
     init_log(&c);
+
+    asn_logger::warn(main_module_name, "Say Warn");
+    asn_logger::trace(main_module_name, "Say Trace"); // don't see this if set AsnLogLevel::Warn to main_module_name
 }
